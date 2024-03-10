2ae9c344cb2b657837b8cb7eaaf5b828eda52d2c
Kolya <160027167+SweetieKitty@users.noreply.github.com>
2024-03-10 10:35:16 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+for ($i = 1; $i <= 100; $i++) {
+    if ($i % 2 == 0) {
+        echo $i . "\n";
+    }
+}
+$numbers = [1, 23, 44, 88, 13, 11, 7 , 8];
+foreach ($numbers as $number) {
+    if ($number % 2 == 0) {
+        echo $number . "\n";
+    }
+}
